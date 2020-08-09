ab04904a8d408ba19e4fe0cf7de8931327eaf710
anjankailash.ippili <anjankailash.ippili@servicenow.com>
2020-08-09 13:02:30 +0530

    Updates

---
+++ precache-manifest.1d808d8251a56c9e23c38aecd2af425a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "797fc90da1b27dff504e",
+    "url": "/movies-series-info/static/css/main.c358c13d.chunk.css"
+  },
+  {
+    "revision": "797fc90da1b27dff504e",
+    "url": "/movies-series-info/static/js/main.797fc90d.chunk.js"
+  },
+  {
+    "revision": "87f7407a67ef306dbf94",
+    "url": "/movies-series-info/static/js/1.87f7407a.chunk.js"
+  },
+  {
+    "revision": "7920a5aea1706981845b",
+    "url": "/movies-series-info/static/js/runtime~main.7920a5ae.js"
+  },
+  {
+    "revision": "826df936f5ed100ce05828719f2bd354",
+    "url": "/movies-series-info/index.html"
+  }
+];
